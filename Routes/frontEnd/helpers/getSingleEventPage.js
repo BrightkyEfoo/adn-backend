62f1afe6b2e8e041a3fd515c1110d3344fb775b8
BrightkyEfoo <108243838+BrightkyEfoo@users.noreply.github.com>
2023-02-21 18:34:28 +0100

    event page single

--- Routes/frontEnd/helpers/getSingleEventPage.js
+++ Routes/frontEnd/helpers/getSingleEventPage.js
@@ -6,6 +6,6 @@ export const getSingleEventPage = (req, res) => {
     console.log(req.params.language)
   SingleEventPage.findOne({ where: { language: req.params.language } })
     // .then(singleEventpage => res.json({ ...singleEventpage.fullGet() }))
-    .then(singleEventpage => res.json({ ...singleEventpage }))
+    .then(singleEventpage => res.json({ page : singleEventpage }))
     .catch(err => res.status(404).json({ ...err }));
 };
