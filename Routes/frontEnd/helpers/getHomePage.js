0f6ad05d4d2d0bbcdfc5ec704afc57a5d47cf9cc
BrightkyEfoo <108243838+BrightkyEfoo@users.noreply.github.com>
2023-02-21 11:27:35 +0100

    essai 2

--- Routes/frontEnd/helpers/getHomePage.js
+++ Routes/frontEnd/helpers/getHomePage.js
@@ -4,13 +4,13 @@ export const getHomePage = (req, res) => {
   console.log('req.params.language', req?.params?.language)
   HomePage.findOne({ where: { language: req.params.language } })
     .then(page => {
-      const pa = page.toJSON()
-      let i = 0
-      for(i = 1 ; i <= 7 ; i++){
-        pa['section'+i] = JSON.parse(pa['section'+i])
-      }
+      // const pa = page.toJSON()
+      // let i = 0
+      // for(i = 1 ; i <= 7 ; i++){
+      //   pa['section'+i] = JSON.parse(pa['section'+i])
+      // }
       console.log('page', page)
-      res.json({ page : pa });
+      res.json({ page : page });
     })
     .catch(err => {
       res.status(404).json({ msg: 'page not found' , err });
