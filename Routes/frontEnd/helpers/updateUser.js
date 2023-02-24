17282ee5041caaed2bd11a9a6ed9cd6496f1a284
BrightkyEfoo <108243838+BrightkyEfoo@users.noreply.github.com>
2023-02-25 00:12:30 +0100

    pwd 4

--- Routes/frontEnd/helpers/updateUser.js
+++ Routes/frontEnd/helpers/updateUser.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 
 export const updateUser = (req, res) => {
   const { id } = req.params;
-  console.log('id', id)
+  console.log('id', id);
   console.log('req.body', req.body);
   const { userId, userSubmission, newPassword, verifyPassword } = req.body; // ce user id la est celui de l'utilisateur qui initie l'operation
   // console.log('req', req)
@@ -18,104 +18,110 @@ export const updateUser = (req, res) => {
           console.log('req.user', req.user);
           return res.status(401).json({ msg: 'unauthorized 1' });
         }
-        if (admin.accessLevel > 2 || admin.id === userToUpdate.id) {
-          if (admin.accessLevel <= 2) {
-            if (
-              'email' in userSubmission ||
-              userSubmission.hasOwnProperty('email')
-            ) {
-              delete userSubmission.email;
-              return res.status(401).json({ mag: 'you are not allowed' });
-            }
-            if (
-              'accessLevel' in userSubmission ||
-              userSubmission.hasOwnProperty('accessLevel')
-            ) {
-              delete userSubmission.accessLevel;
-              return res.status(401).json({ mag: 'you are not allowed' });
-            }
+        if (admin.accessLevel <= 2) {
+          if (
+            'email' in userSubmission ||
+            userSubmission.hasOwnProperty('email')
+          ) {
+            delete userSubmission.email;
+            return res.status(401).json({ mag: 'you are not allowed' });
           }
-          //ici
+          if (
+            'accessLevel' in userSubmission ||
+            userSubmission.hasOwnProperty('accessLevel')
+          ) {
+            delete userSubmission.accessLevel;
+            return res.status(401).json({ mag: 'you are not allowed' });
+          }
+        }
+        //ici
 
-          //fin ici
-          if (parseInt(id) === userId) {
-            if (newPassword) {
-              if (!verifyPassword) {
-                return res
-                  .status(401)
-                  .json({
-                    msg: 'you should provide your password in the verify password field',
-                  });
-              } else {
-                bcrypt
-                  .compare(verifyPassword, userToUpdate.password)
-                  .then(_ => {
-                    if (!_) {
-                      // userToUpdate.update({loginTryCounter : userToUpdate.loginTryCounter - 1})
-                      return res
-                        .status(401)
-                        .json({
-                          msg: 'wrong password filled on verify password field',
-                        });
-                    } else {
-                      bcrypt.hash(newPassword,10).then(hash => {
-                        userToUpdate
-                          .update({ ...userSubmission, password: hash })
-                          .then(userUpdated => {
-                            let temp = userUpdated.toJSON();
-                            delete temp.password;
-                            return res.json({ msg: 'success', user: temp });
-                          });
-                      });
-                    }
-                  });
-              }
+        //fin ici
+        if (parseInt(id) === userId) {
+          if (newPassword) {
+            if (!verifyPassword) {
+              return res.status(401).json({
+                msg: 'you should provide your password in the verify password field',
+              });
             } else {
-              let Temp = {...userSubmission}
-              if(Temp.password){
-                delete Temp.password
-              }
+              bcrypt.compare(verifyPassword, userToUpdate.password).then(_ => {
+                if (!_) {
+                  // userToUpdate.update({loginTryCounter : userToUpdate.loginTryCounter - 1})
+                  return res.status(401).json({
+                    msg: 'wrong password filled on verify password field',
+                  });
+                } else {
+                  bcrypt.hash(newPassword, 10).then(hash => {
+                    userToUpdate
+                      .update({ ...userSubmission, password: hash })
+                      .then(userUpdated => {
+                        let temp = userUpdated.toJSON();
+                        delete temp.password;
+                        return res.json({ msg: 'success', user: temp });
+                      });
+                  });
+                }
+              });
+            }
+          } else {
+            // let Temp = { ...userSubmission };
+            // if (Temp.password) {
+            //   delete Temp.password;
+            // }
+            if (userSubmission.password) {
+              bcrypt.hash(userSubmission.password, 10).then(hash => {
+                userToUpdate
+                  .update({ ...userSubmission, password: hash })
+                  .then(userUpdated => {
+                    let temp = userUpdated.toJSON();
+                    delete temp.password;
+                    return res.json({ msg: 'success', user: temp });
+                  });
+              });
+            } else {
+              let Temp = { ...userSubmission };
+              delete Temp.password;
               userToUpdate.update({ ...Temp }).then(userUpdated => {
                 let temp = userUpdated.toJSON();
                 delete temp.password;
                 return res.json({ msg: 'successfully updated', user: temp });
               });
             }
+          }
+        } else {
+          if (req.user.accessLevel <= 2) {
+            return res.status(401).json({
+              msg: 'you are not allowed to set password of another user',
+            });
           } else {
-            if (req.user.accessLevel <= 2) {
-              return res
-                .status(401)
-                .json({
-                  msg: 'you are not allowed to set password of another user',
-                });
+            if (newPassword) {
+              bcrypt.hash(newPassword, 10).then(hash => {
+                userToUpdate
+                  .update({ ...userSubmission, password: hash })
+                  .then(userUpdated => {
+                    let temp = userUpdated.toJSON();
+                    delete temp.password;
+                    return res.json({ msg: 'success', user: temp });
+                  });
+              });
+            } else if (userSubmission.password) {
+              bcrypt.hash(userSubmission.password, 10).then(hash => {
+                userToUpdate
+                  .update({ ...userSubmission, password: hash })
+                  .then(userUpdated => {
+                    let temp = userUpdated.toJSON();
+                    delete temp.password;
+                    return res.json({ msg: 'success', user: temp });
+                  });
+              });
             } else {
-              if (newPassword) {
-                bcrypt.hash(newPassword,10).then(hash => {
-                  userToUpdate
-                    .update({ ...userSubmission, password: hash })
-                    .then(userUpdated => {
-                      let temp = userUpdated.toJSON();
-                      delete temp.password;
-                      return res.json({ msg: 'success', user: temp });
-                    });
-                });
-              } else if(userSubmission.password){
-                bcrypt.hash(userSubmission.password,10).then(hash => {
-                  userToUpdate
-                    .update({ ...userSubmission, password: hash })
-                    .then(userUpdated => {
-                      let temp = userUpdated.toJSON();
-                      delete temp.password;
-                      return res.json({ msg: 'success', user: temp });
-                    });
-                });
-              }else {
-                userToUpdate.update({ ...userSubmission }).then(userUpdated => {
-                  let temp = userUpdated.toJSON();
-                  delete temp.password;
-                  return res.json({ msg: 'successfully updated', user: temp });
-                });
-              }
+              let Temp = {...userSubmission}
+              delete Temp.password
+              userToUpdate.update({ ...Temp }).then(userUpdated => {
+                let temp = userUpdated.toJSON();
+                delete temp.password;
+                return res.json({ msg: 'successfully updated', user: temp });
+              });
             }
           }
         }
