0ddc9d1bf3bbaa213123e5c413c18a717a7c2178
BrightkyEfoo <108243838+BrightkyEfoo@users.noreply.github.com>
2023-03-10 16:32:35 +0100

    doctors update

--- Models/Doctors.js
+++ Models/Doctors.js
@@ -32,7 +32,7 @@ const DoctorModel = sequelize => {
         type: DataTypes.STRING,
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(2000),
       },
     },
     {
